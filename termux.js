4997e9b04d2d3c91f9776232d3c922e0a6242828
jbs-17 <jatibangun17@gamil.com>
2025-08-04 18:27:49 +0700

    a

--- termux.js
+++ termux.js
@@ -1,29 +1,9 @@
+import { spawn } from 'child_process';
+
+const ssh = spawn('ssh', ['-p', '22', 'hewlett-packard@localhost']);
 
 
-const { Client } = require('ssh2');
-
-const conn = new Client();
-conn.on('ready', () => {
-  console.log('Client :: ready');
-  conn.shell((err, stream) => {
-    if (err) throw err;
-    stream.on('close', () => {
-      console.log('Stream :: close');
-      
-    }).on('data', (data) => {
-      console.log({data: data + ' '});
-    });
-    stream.write('pwd\n');
-    setTimeout(() => {
-      stream.write('echo $HOME\n')
-    }, 3000);
-    setTimeout(() => {
-      stream.write('exit\n')
-    }, 6000);
-  });
-}).connect({
-    host: "localhost",
-    port: 9999,
-    username: "u0_379",
-    password: "1248"
+ssh.stdout.on('data', (chunk)=>{
+  console.log(chunk.toString());
 });
+ssh.stdin.write('node', )
